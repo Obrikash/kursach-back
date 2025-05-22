6db46fed8bf7bf22c97d36a5d7c96673ba3fcc6d
Nikita <obrikash@gmail.com>
2025-05-22 18:42:05 +0300

    enabling CORS for my front server

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ include .envrc
 ## run/api: run the cmd/api application
 .PHONY: run/api
 run/api:
-	@go run ./cmd/api -db-dsn=${SWIMMING_POOL_DSN}
+	@go run ./cmd/api -db-dsn=${SWIMMING_POOL_DSN} -cors-trusted-origins="http://localhost:5173"
 
 .PHONY: db/psql
 db/psql:
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,18 +15,18 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/pools", app.listPoolsHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/pool", app.mostProfitPoolHandler)
 
-	router.HandlerFunc(http.MethodGet, "/v1/users/trainers", app.listTrainersHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/pools/trainers", app.listTrainersForPoolsHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/users/trainers", app.requireAuthenticatedUser(app.listTrainersHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/pools/trainers", app.requireAuthenticatedUser(app.listTrainersForPoolsHandler))
 	router.HandlerFunc(http.MethodPost, "/v1/pools/trainers", app.requireAuthenticatedUser(app.attachTrainerToPoolHandler))
-	router.HandlerFunc(http.MethodGet, "/v1/users/trainers/profit", app.profitOfTrainers)
+	router.HandlerFunc(http.MethodGet, "/v1/users/trainers/profit", app.requireAuthenticatedUser(app.profitOfTrainers))
 
-	router.HandlerFunc(http.MethodGet, "/v1/groups", app.listGroupsHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/groups", app.addGroupToPoolHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/groups", app.requireAuthenticatedUser(app.listGroupsHandler))
+	router.HandlerFunc(http.MethodPost, "/v1/groups", app.requireAuthenticatedUser(app.addGroupToPoolHandler))
 
-	router.HandlerFunc(http.MethodGet, "/v1/subscriptions", app.listSubscriptionsHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/subscriptions", app.requireAuthenticatedUser(app.listSubscriptionsHandler))
 
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
-	return app.recoverPanic(app.authenticate(router))
+	return app.recoverPanic(app.enableCORS((app.authenticate(router))))
 }
